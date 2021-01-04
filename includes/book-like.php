d4114d4f8b3d7317c75c8d3c653d560b36c72449
chandima <chandimaofficial@gmail.com>
2021-01-04 20:53:42 +0530

    book like and unlike button work perfectly.

--- includes/book-like.php
+++ includes/book-like.php
@@ -4,34 +4,28 @@ if(!isset($_SESSION['userId'])) {
   header("location:login.php?error=LoginFirst"); 
 }
 
-
 $userId= $_SESSION['userId'];
 $bookId = $_GET["message"];
-$borrowDate =  date("Y/m/d");
-$returnDate = date("Y/m/d" ,  strtotime('+7 days'));
-// get date and time of borrow in utc form
 
 require_once 'dbh-inc.php';
+$liked =1;
+likeBook($conn,$userId,$bookId,$liked);
 
-// echo  $borrowDate . "<br>".$returnDate . "<br>". $bookId. "<br>".$userId ;
-
-borrowBook($conn,$userId,$bookId,$borrowDate,$returnDate);
-
-function borrowBook($conn,$userId,$bookId,$borrowDate,$returnDate){
+function likeBook($conn,$userId,$bookId,$liked){
     $stmt = mysqli_stmt_init($conn);
-    $sql = "INSERT INTO borrowbooks(userId,bookId,borrowDate,returnDate) VALUES (?,?,?,?);";
+    $sql = "INSERT INTO booklikes (userId,bookId,liked) VALUES (?,?,?);";
   
    if (!mysqli_stmt_prepare($stmt,$sql)){
-       header("location: ../bookBorrow.php?error=stmtFaild4bookBorrow");
+       header("location: ../one-book-view.php?error=stmtFaild4bookLike");
        exit();
             }
 
-   mysqli_stmt_bind_param($stmt,"ssss",$userId,$bookId,$borrowDate,$returnDate);
+   mysqli_stmt_bind_param($stmt,"sss",$userId,$bookId,$liked);
    mysqli_stmt_execute($stmt); 
 
    mysqli_stmt_close($stmt);
-
-   header("location: ../bookSearch.php?error=successfullyBorrowed");
+   header('location:../one-book-view.php?message='.$bookId.' &error=successfullyLiked');
+//    header("location: ../bookSearch.php?error=successfullyBorrowed");
    exit();}
 
 
--- like-button.php
+++ like-button.php
@@ -5,35 +5,37 @@ $bookId = $_GET["message"];
 
 require_once 'includes/dbh-inc.php';
 
-$sql = "SELECT * FROM borrowbooks WHERE userId =  '$userId'  and  bookId =  '$bookId'";
+$sql = "SELECT * FROM booklikes WHERE userId =  '$userId'  and  bookId =  '$bookId'";
 $result = $conn->query($sql);
 
-$returnedOrNot=1;
+$likedOrNot=0;
 
 while($row = $result->fetch_assoc()) {
 
-    if($row["returned"]==0){
+    if($row["liked"]==0){
         // echo 'not returned';
-        $returnedOrNot=0;
-    }elseif($row["returned"]==1){
+        $likedOrNot=0;
+    }elseif($row["liked"]==1){
         // echo 'returned';
-        $returnedOrNot=1;
+        $likedOrNot=1;
     }
   }
 
 
 
-    if($returnedOrNot==1){
+    if($likedOrNot==0){
         echo '
         <div class="col-6 col-lg-3">
-                
+        <a href="includes/book-like.php?message='.$bookId.' ">
         <button type="submit" class="mybtnl btn btn-primary">Like</button>
+        </a>
     </div>';
     }
     else{
         echo '
         <div class="col-6 col-lg-3">
-                
-        <button type="submit" class="mybtnl btn btn-primary">Like</button>
+        <a href="includes/book-unlike.php?message='.$bookId.' ">   
+        <button type="submit" class="mybtnl btn btn-primary">Unlike</button>
+        </a>
     </div>';   
 }
